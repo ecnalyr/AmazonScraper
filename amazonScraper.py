357893068831122b741a89537015800c2da66117
Jason Rylance <ecnalyr@gmail.com>
2012-11-29 11:02:00 -0600

    Added get4StarsAndUpFromNthCategory
    Also performed some refactoring

--- amazonScraper.py
+++ amazonScraper.py
@@ -5,15 +5,19 @@ def getAmazonSearchPage(searchTerm):
     '''Returns html data from an Amazon search page'''
     BaseSearchUrl = "http://www.amazon.com/s/?url=search-alias%3Daps&field-keywords="
     SearchUrlWithSearchTerm = BaseSearchUrl + searchTerm
-    resultPage = urllib2.urlopen(SearchUrlWithSearchTerm)
+    resultPage = getHtml(SearchUrlWithSearchTerm)
     return resultPage
 
 
 def scrapeAmazon(searchTerm):
     '''Returns BeautifulSoup formatted html data from Amazon'''
     resultPage = getAmazonSearchPage(searchTerm)
-    soupResults = BeautifulSoup(resultPage)
-    return soupResults
+    return soupify(resultPage)
+
+
+def soupify(htmlData):
+    '''Expects HTML data from a web page'''
+    return BeautifulSoup(htmlData)
 
 
 def thumbnail(soupData, nthItem):
@@ -45,7 +49,32 @@ def nthCategory(categories, nthItem):
     representing which category you would like to return'''
     if nthItem > len(categories):
         return "Your nth value is too large for the number of categories in the list."
-    return categories[nthItem - 1]
+    category = categories[nthItem - 1]
+    return categoryWithoutMarkUp(category)
+
+
+def categoryWithoutMarkUp(categoryMarkUp):
+    '''Expects a single category (like from nthCategory)'''
+    categoryStringDiv = categoryMarkUp.find("span", {"class": "refinementLink"})
+    name = categoryStringDiv.string
+    categoryCountDiv = categoryStringDiv.find_next("span", {"class": "narrowValue"})
+    quantity = categoryCountDiv.string
+    link = categoryMarkUp.find_next('a')['href']
+    return link + " " + name + quantity
+
+
+def fourStarsAndUp(parentLink):
+    '''Expects a link to an Amazon search result page that has been sorted by category'''
+    parentPage = soupify(getHtml(parentLink))
+    starLinksOfPage = parentPage.find(text= "Avg. Customer Review").parent
+    return starLinksOfPage.find_next('ul').find_next('li').find_next('a')['href']
+    # After <h2>Avg. Customer Review</h2>, find the first ul, then find the first li, then find the first <a>
+
+def getHtml(url):
+    '''Returns html data from a given url'''
+    return urllib2.urlopen(url)
+
+
 
 
 class amazonScrape:
@@ -71,8 +100,15 @@ class amazonScrape:
         print self.categories
 
     def getNthCategory(self, nthItem):
-        '''Returns nth category'''
-        print nthCategory(self.categories, nthItem)
+        '''Returns nth category without markup'''
+        return nthCategory(self.categories, nthItem)
+
+    def get4StarsAndUpFromNthCategory(self, nthCategory):
+        '''Returns a link to the 4 star and up page for nth category'''
+        realNumber = nthCategory
+        category = self.getNthCategory(realNumber)
+        parentLink = category.split()[0]
+        return fourStarsAndUp(parentLink)
 
 
 ##### Example output
@@ -80,11 +116,9 @@ userInput = raw_input('Enter a search term: ')
 scrape = amazonScrape(userInput)
 scrape.getImage(1)
 scrape.getFirstItemsCategory()
-scrape.getNthCategory(2)
+scrape.getNthCategory(1)
+print "4 star and up link = " + scrape.get4StarsAndUpFromNthCategory(1)
 # scrape.getCategoryList()
 
-
-
-
-
-
+# 4 star and up:
+# After <h2>Avg. Customer Review</h2>, find the first ul, then find the first li, then find the first <a>
